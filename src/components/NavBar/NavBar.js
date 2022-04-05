9de96c13badd122e59f6af18d2934573a334b28c
azizurrahman-zero <azizurrahman.zero@gmail.com>
2022-04-05 13:03:57 +0600

    custom link added

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
 import './NavBar.css'
 
 const NavBar = () => {
     return (
         <nav className='nav-bar'>
-            <Link to="/">Home</Link>
-            <Link to="/reviews">Reviews</Link>
-            <Link to="/dashboard">Dashboard</Link>
-            <Link to="/blogs">Blogs</Link>
-            <Link to="/about">About</Link>
+            <CustomLink to="/">Home</CustomLink>
+            <CustomLink to="/reviews">Reviews</CustomLink>
+            <CustomLink to="/dashboard">Dashboard</CustomLink>
+            <CustomLink to="/blogs">Blogs</CustomLink>
+            <CustomLink to="/about">About</CustomLink>
         </nav>
     );
 };
