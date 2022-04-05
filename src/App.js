5092d24f362fb58fd3bdb38661ce085be0694b48
azizurrahman-zero <azizurrahman.zero@gmail.com>
2022-04-05 11:17:15 +0600

    not found component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
 import Blogs from './components/Blogs/Blogs';
 import Dashboard from './components/Dashboard/Dashboard';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
 import Reviews from './components/Reviews/Reviews';
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
         <Route path='/reviews' element={<Reviews></Reviews>}></Route>
         <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css'
+
+const NotFound = () => {
+    return (
+        <div className='not-found'>
+            <img src="/images/not-found.png" alt="" />
+            <Link to="/">Back to Homepage</Link>
+        </div>
+    );
+};
+
+export default NotFound;
